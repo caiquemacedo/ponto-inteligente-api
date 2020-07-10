4623eb9fd8e8cb395bc18a5d2a104a5174a5421e
caiquemacedotargetdata <caique.macedo@targetdata.com.br>
2020-07-09 21:10:48 -0300

    Adicionando uma dependencia do JUnit

--- src/test/java/com/kazale/pontointeligente/api/PontoInteligenteApplicationTest.java
+++ src/test/java/com/kazale/pontointeligente/api/PontoInteligenteApplicationTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
-class PontoInteligenteApplication {
+class PontoInteligenteApplicationTest {
 
 	@Test
 	void contextLoads() {
